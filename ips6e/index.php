6bb2215dec347adb007c4f4038a050a05eaaaca1
Nicholas Horton <nicholasjhorton@gmail.com>
2018-06-11 14:07:42 -0400

    Adding old files for Bonnie.

---
+++ ips6e/index.php
@@ -0,0 +1,57 @@
+<title>Introduction to the Practice of Statistics (6th edition) in R</title>
+  <div id="mainContent" style="padding:25px 0 0 0;">
+		<p class="name">Introduction to the Practice of Statistics (6th edition) in R</p>
+
+<p>These files are intended to help 
+describe how to undertake analyses introduced as examples in the first chapters of the Sixth Edition of <a href="http://bcs.whfreeman.com/ips6e/" target="_blank">Introduction to the Practice of Statistics</a> (2007), 
+the excellent text by David Moore, George McCabe and Bruce Craig, using R.
+
+Similar materials are available for the 
+<a href="http://www.amherst.edu/~nhorton/sleuth">second</a> and
+<a href="http://www.math.smith.edu/~nhorton/sleuth3">third</a>
+editions of
+the <i>Statistical Sleuth</i>.
+</p>
+
+<p>
+This work leverages efforts undertaken by <a href="http://www.mosaic-web.org" target="_blank">Project MOSAIC</a>, an NSF-funded initiative to improve the teaching of statistics, calculus, science and computing in the undergraduate curriculum. In particular, we utilize the 
+<a href="http://cran.r-project.org/web/packages/mosaic" target="_blank">mosaic</a> package, which was written to simplify the use of R for introductory statistics courses. 
+A short summary of the R needed to teach introductory statistics can be found in the mosaic package vignette. located 
+<a href="http://cran.r-project.org/web/packages/mosaic">here</a>.</p>
+
+<p>Other R packages used include MASS, agricolae, car, gmodels, leaps and xtable.</p>
+<p>
+We have include both formatted pdf files as well as the original knitr files which were used to generate the output. <a href="http://yihui.name/knitr/">Knitr</a> is an elegant, flexible and fast means to undertake reproducible analysis
+and dynamic report generation within R and <a href="http://www.rstudio.org" target="_blank">RStudio</a>.  Comments, suggestions and corrections welcomed.
+
+<p>
+Nicholas Horton and Ben Baumer (last updated January 12, 2014)</p>
+		<p class="header">pdf and knitr files</p>
+<ul>
+<li>Chapter 1 (Looking at Data Distributions): <a href="chapter01.pdf">pdf</a> <a href="chapter01.Rnw">knitr</a></li>
+<li>Chapter 2 (Looking at Data Relationships): <a href="chapter02.pdf">pdf</a> <a href="chapter02.Rnw">knitr</a></li>
+<li>Chapter 3 (Producing Data): <a href="chapter03.pdf">pdf</a> <a href="chapter03.Rnw">knitr</a></li>
+<li>Chapter 4 (Probability): <a href="chapter04.pdf">pdf</a> <a href="chapter04.Rnw">knitr</a></li>
+<li>Chapter 5 (Sampling Distributions): <a href="chapter05.pdf">pdf</a> <a href="chapter05.Rnw">knitr</a></li>
+<li>Chapter 6 (Introduction to Inference): <a href="chapter06.pdf">pdf</a> <a href="chapter06.Rnw">knitr</a></li>
+<li>Chapter 7 (Inference for Means): <a href="chapter07.pdf">pdf</a> <a href="chapter07.Rnw">knitr</a></li>
+<li>Chapter 10 (Inference for Regression): <a href="chapter10.pdf">pdf</a> <a href="chapter10.Rnw">knitr</a></li>
+<li>Chapter 11 (Multiple Regression): <a href="chapter11.pdf">pdf</a> <a href="chapter11.Rnw">knitr</a></li>
+<li>Chapter 12 (One-Way ANOVA): <a href="chapter12.pdf">pdf</a> <a href="chapter12.Rnw">knitr</a></li>
+<li>Chapter 16 (Resampling, Bootstrapping and Permutation Testing): <a href="chapter16.pdf">pdf</a> <a href="chapter16.Rnw">knitr</a></li>
+<li>Additional file needed to compile: <a href="language.sty">language.sty</a></li>
+<li>Additional files which also may needed to compile: 
+<a href="xcolor.sty">xcolor.sty</a>, 
+<a href="xstring.sty">xstring.sty</a>, 
+and/or
+<a href="xstring.tex">xstring.tex</a> 
+</li>
+</ul>
+
+
+  </div>
+  <!-- end #mainContent -->
+
+  <!-- This clearing element should immediately follow the #mainContent div in order to force the #container div to contain all child floats -->
+  <br class="clearfloat" />
+  <div id="footer">
